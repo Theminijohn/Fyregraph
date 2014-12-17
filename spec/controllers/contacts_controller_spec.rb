3aa3d832c79a907f1a4941c1067f710710e1b367
The Mini John <admin@phoenix-prime.gr>
2014-12-17 14:34:25 +0100

    Nested Resource Spec

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
 
 describe ContactsController do
+
+  # Devise Log in
+  login_user
+
+  # Show --------------------------------------------
+  describe 'GET #show' do
+
+    it 'renders the :show template for the Contact' do
+      project = create(:project)
+      contact = create(:contact, project: project)
+      get :show, id: contact, project_id: project.id
+      expect(response).to render_template :show
+    end
+
+  end
+
 end
